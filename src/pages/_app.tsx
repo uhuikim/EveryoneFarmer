ea3b3de1a6a4421e56a0ad17277c245e2b96d56f
blockodyssey-uhui <reah@blockodyssey.io>
2023-03-16 23:06:21 +0900

    feat : emotion nomalize 추가 및 글로벌 스타일 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import type { AppProps } from 'next/app';
+import { Global } from '@emotion/react';
+import globalStyle from 'styles/globals';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Global styles={globalStyle} />
+            <Component {...pageProps} />
+        </>
+    );
 }
---
+++ src/styles/globals.ts
@@ -0,0 +1,6 @@
+import { css } from '@emotion/react';
+import normalize from 'emotion-normalize';
+
+export default css`
+    ${normalize}
+`;
